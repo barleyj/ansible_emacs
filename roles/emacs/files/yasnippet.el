bab8a6e442566d067dc49c3df9db0a21deaffcc3
Jayson Barley <jbarley@opengov.com>
2019-09-08 14:42:26 -0700

    Update yasnippet config for new version

--- roles/emacs/files/yasnippet.el
+++ roles/emacs/files/yasnippet.el
@@ -1,10 +1,10 @@
 (add-to-list 'load-path
-             "~/.emacs.d/elpa/yasnippet-0.8.0")
+             "~/.emacs.d/plugins/yasnippet")
 
 (require 'yasnippet)
 (yas-global-mode 1)
 
 (setq yas-snippet-dirs
-      '("~/.emacs.d/snippets"                 ;; personal snippets
-        "~/.emacs.d/elpa/yasnippet-0.8.0/snippets"         ;; the default collection
+      '("~/.emacs.d/snippets"                   ;; personal snippets
+        "~/.emacs.d/plugins/yasnippet/snippets" ;; the default collection
         ))
